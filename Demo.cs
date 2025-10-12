17d883dc5203f27d142892b1acca5478dc7e61d8
Nithin-21 <ravurinithin14@gmail.com>
2025-10-12 21:06:26 +0530

    first commit on 12-

--- Demo.cs
+++ Demo.cs
@@ -137,7 +137,7 @@ namespace DemoAPI
                           {
                               pdf.Qty = Convert.ToInt32(itm["storeQty"]) > 0 ? Convert.ToInt32(itm["storeQty"]) : 0;
                           }*/
-                        pdf.Qty=Convert.ToInt32(itm["storeQty"]);
+                        pdf.Qty = Convert.ToInt32(itm["storeQty"]);
 
                         pdf.Price = Convert.ToDecimal(itm["priceperUnit"]);
                         fnf.Price = Convert.ToDecimal(itm["priceperUnit"]);
@@ -155,22 +155,22 @@ namespace DemoAPI
                         fnf.region = "";
                         fnf.country = "";
                         fnf.pcat = itm["departmentName"].ToString();
-                        
-                            if (pdf.Qty > 0 && pdf.Price > 0)
-                            {
-                                pm.Add(pdf);
-                                fullname.Add(fnf);
-                            }
-                       
-                            if (pdf.Price > 0)
-                            {
-                                pm.Add(pdf);
-                                fullname.Add(fnf);
-                            }
+
+                        if (pdf.Qty > 0 && pdf.Price > 0)
+                        {
+                            pm.Add(pdf);
+                            fullname.Add(fnf);
+                        }
+
+                        if (pdf.Price > 0)
+                        {
+                            pm.Add(pdf);
+                            fullname.Add(fnf);
                         }
                     }
                 }
-            
+            }
+
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
@@ -183,11 +183,73 @@ namespace DemoAPI
                                      .Select(g => g.First())
                                      .ToList();
 
-            GenerateCSV.GenerateCSVFile(pm,"Product",storeid,BaseUrl);
+            GenerateCSV.GenerateCSVFile(pm, "Product", storeid, BaseUrl);
             GenerateCSV.GenerateCSVFile(fullname, "FullName", storeid, BaseUrl);
             Console.WriteLine("Product file generated for RetailZAPI " + storeid);
             Console.WriteLine("FullName file generated for RetailZAPI " + storeid);
 
+
+            // 12-10-2025   new changes for testing the git hub repo   
+            #region      demo code for commit changes    
+              public Demo(int storeid, decimal tax, string baseurl, string authkey, string token)
+            {
+                Console.WriteLine("Generating Product File For Store: " + storeid);
+                ResposeToCSV(storeid, tax, baseurl, authkey, token);
+            }
+
+            public List<JArray> GetResponse(string baseurl, string authkey, string token)
+            {
+                List<JArray> itemList = new List<JArray>();
+                try
+                {
+                    var client1 = new RestClient(baseurl + "/Item/?page=1&Size=500");
+                    var request1 = new RestRequest("", Method.Get);
+
+                    request1.AddHeader("AuthKey", authkey);
+                    request1.AddHeader("Token", token);
+                    var response = client1.Execute(request1);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string responseContent = response.Content;
+                        var pJson = JObject.Parse(responseContent);
+
+                        var totalCountsToken = pJson["data"]?["totalCounts"];
+                        int totalCounts = totalCountsToken != null ? (int)totalCountsToken : 0;
+                        int itemsPerPage = 500;
+                        int totalPages = (int)Math.Ceiling((double)totalCounts / itemsPerPage);
+
+                        for (int page = 1; page <= totalPages; page++)
+                        {
+                            var client = new RestClient(baseurl + "/Item/?page=" + page + "&Size=" + itemsPerPage);
+                            var request = new RestRequest("", Method.Get);
+
+                            request.AddHeader("AuthKey", authkey);
+                            request.AddHeader("Token", token);
+                            var pageResponse = client.Execute(request);
+
+                            if (pageResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                            {
+                                string pageResponseContent = pageResponse.Content;
+
+
+                                var pageJson = JObject.Parse(pageResponseContent);
+                                var dataToken = pageJson["data"];
+                                var data = dataToken["data"];
+
+                                if (data is JArray jArray)
+                                {
+                                    itemList.Add(jArray);
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                #endregion
+
+
+            }
         }
     }
-}
